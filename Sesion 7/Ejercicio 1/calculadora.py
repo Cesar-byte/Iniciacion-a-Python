913fea8ff2aeef0957e6eb431df357e9da796f42
Cesar Garcia Moreno <01cesargm@gmail.com>
2022-09-03 12:59:13 +0200

    Ejercicio 1 completo

---
+++ Sesion 7/Ejercicio 1/calculadora.py
@@ -0,0 +1,23 @@
+def sumar (a,b):
+    '''
+     Devuelve el resultado de sumar a y b (a+b)
+    '''    
+    return a+b
+
+def restar (a,b):
+    '''
+     Devuelve el resultado de restar a y b (a-b)
+    '''    
+    return a - b
+
+def multiplicar (a,b):
+    '''
+     Devuelve el resultado de multiplicar a y b (a*b)
+    '''    
+    return a*b
+
+def dividir (a,b):
+    '''
+     Devuelve el resultado de dividir a y b (a/b)
+    '''    
+    return a/b
---
+++ Sesion 7/Ejercicio 1/main.py
@@ -0,0 +1,10 @@
+import calculadora as calc
+
+def main():
+    print("suma 3+4:",calc.sumar(3,4))
+    print("resta 3-4:",calc.restar(3,4))
+    print("multiplicar 3*4:",calc.multiplicar(3,4))
+    print("dividir 3/4:",calc.dividir(3,4))
+
+
+main()
